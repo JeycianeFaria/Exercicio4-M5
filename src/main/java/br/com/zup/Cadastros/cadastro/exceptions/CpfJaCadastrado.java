4ca77938071531fa5755d24862d52f498f8c4746
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-08 20:12:35 -0300

    Modelar classe para cpf já cadastrado

---
+++ src/main/java/br/com/zup/Cadastros/cadastro/exceptions/CpfJaCadastrado.java
@@ -0,0 +1,9 @@
+package br.com.zup.Cadastros.cadastro.exceptions;
+
+public class CpfJaCadastrado extends RuntimeException{
+
+    public CpfJaCadastrado(String message) {
+        super(message);
+    }
+
+}
