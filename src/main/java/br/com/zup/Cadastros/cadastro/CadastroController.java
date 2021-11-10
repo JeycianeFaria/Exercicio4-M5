3913984e53bef0150ec3b16f1a7bdec22bf0084a
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-10 08:27:19 -0300

    Implementa componente de conversão de classes modelMapper

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroController.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroController.java
@@ -2,6 +2,7 @@ package br.com.zup.Cadastros.cadastro;
 
 import br.com.zup.Cadastros.cadastro.dtos.CadastroDTO;
 import br.com.zup.Cadastros.cadastro.dtos.SaidaCadastrosDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +16,9 @@ import java.util.List;
 public class CadastroController {
     @Autowired
     private CadastroService cadastroService;
+    @Autowired
+    private ModelMapper modelMapper;
 
-    /*
-    todo  1 - crie um metodo para cadastrar uma pessoa.
-      Para um cadastro todo os campos são obrigatórios EXCETO o campo dataDoCadastro que deve ser preenchido pelo proprio sistema e o client não deve saber da existencia desse campo
-     todo 2 - Faça um metodo que retorna a lista inteira de cadastros ou filtrado por cadastros que moram sozinhos, que tem pet ou por idade.
-     nessa lista deve ser retornado apenas os campos ID, NOME e SOBRENOME.
-     todo 3 - faça um metodo para DELETAR um cadastro por id.
-     todo 4 - faça um metodo que retorna TODOS os dados de um usuario pesquisado pelo ID.
-     */
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
---
+++ src/main/java/br/com/zup/Cadastros/cadastro/componentes/ConversorModelMapper.java
@@ -0,0 +1,15 @@
+package br.com.zup.Cadastros.cadastro.componentes;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConversorModelMapper {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+}
